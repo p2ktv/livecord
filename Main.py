fd96e1a81df17ed2b33f1d93eeb57aba92b64c8c
Paul <55191289+xezzz@users.noreply.github.com>
2021-02-16 09:22:31 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,16 +12,13 @@ if __name__ == "__main__":
     i.guilds = True
     i.emojis = True
 
-
     args = {
-        "command_prefix": "%",
+        "command_prefix": "%", # doesn't really matter, since there aren't any commands
         "case_senitive": True,
         "max_messages": 1000,
         "intents": i,
         "chunk_guilds_at_startup": True
     }
 
-
     bot = TwitchBot(**args)
-
-    bot.run(Config.TOKEN)
+    bot.run()
