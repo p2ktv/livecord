e9b8f4b70f0d1cd3525240f486967e6bf1fbe14a
Paul <55191289+xezzz@users.noreply.github.com>
2021-02-16 23:15:49 +0100

    Update Twitch.py

--- Twitch/Twitch.py
+++ Twitch/Twitch.py
@@ -29,32 +29,22 @@ async def get_twitch_data(channel_name):
 
 
     channel_url = "https://api.twitch.tv/helix/search/channels?query={}".format(channel_name)
-    channel_data = None
-    for x in r.get(url=channel_url, headers=headers).json()["data"]:
-        if x["display_name"].lower() != channel_name.lower():
-            pass
-        else:
-            channel_data = x
+    channel_data = [x for x in r.get(url=channel_url, headers=headers).json()["data"] if x[["display_name"].lower()] != channel_name.lower()][0]
 
+    
     if channel_data["is_live"] is False:
         return None, None, None # return none if the channel isn't live
 
+    
     channel_id = channel_data["id"]
 
+    
     stream_url = "https://api.twitch.tv/helix/streams?user_id={}".format(channel_id)
-    stream_data = None
-    for y in r.get(url=stream_url, headers=headers).json()["data"]:
-        if y["user_id"] != str(channel_id):
-            pass
-        else:
-            stream_data = y
+    stream_data = [y for y in r.get(url=stream_url, headers=headers).json()["data"] if y["user_id"] != str(channel_id)][0]
 
+    
     user_icon_url = "https://api.twitch.tv/helix/users?id={}".format(channel_id)
-    icon_url = None
-    for z in r.get(url=user_icon_url, headers=headers).json()["data"]:
-        if z["id"] != str(channel_id):
-            pass
-        else:
-            icon_url = z["profile_image_url"]
+    icon_url = [z["profile_image_url"] for z in r.get(url=user_icon_url, headers=headers).json()["data"] if z["id"] != str(channel_id)][0]
 
+    
     return channel_data, stream_data, icon_url
