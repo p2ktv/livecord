3b8450320bd76cc4033ca0a37b03056d59aae575
Paul <55191289+xezzz@users.noreply.github.com>
2021-03-12 15:48:21 +0100

    Major fix

--- Twitch/Twitch.py
+++ Twitch/Twitch.py
@@ -30,7 +30,7 @@ async def get_twitch_data(channel_name):
 
 
     channel_url = "{}/search/channels?query={}".format(BASE, channel_name)
-    channel_data = [x for x in r.get(url=channel_url, headers=headers).json()["data"] if x["display_name"].lower() != channel_name.lower()][0]
+    channel_data = [x for x in r.get(url=channel_url, headers=headers).json()["data"] if x["display_name"].lower() == channel_name.lower()][0]
 
     
     if channel_data["is_live"] is False:
@@ -41,11 +41,11 @@ async def get_twitch_data(channel_name):
 
     
     stream_url = "{}/streams?user_id={}".format(BASE, channel_id)
-    stream_data = [y for y in r.get(url=stream_url, headers=headers).json()["data"] if y["user_id"] != str(channel_id)][0]
+    stream_data = [y for y in r.get(url=stream_url, headers=headers).json()["data"] if y["user_id"] == str(channel_id)][0]
 
     
     user_icon_url = "{}/users?id={}".format(BASE, channel_id)
-    icon_url = [z["profile_image_url"] for z in r.get(url=user_icon_url, headers=headers).json()["data"] if z["id"] != str(channel_id)][0]
+    icon_url = [z["profile_image_url"] for z in r.get(url=user_icon_url, headers=headers).json()["data"] if z["id"] == str(channel_id)][0]
 
     
     return channel_data, stream_data, icon_url
